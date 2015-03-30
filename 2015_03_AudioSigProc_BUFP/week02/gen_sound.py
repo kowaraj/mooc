e325c57bebdf14a0b19ac78890fb3795e1666e43
kowaraj <kowaraj4stuff@gmail.com>
2015-03-30 14:21:31 +0200

    added complex sinusoid

--- 2015_03_AudioSigProc_BUFP/week02/gen_sound.py
+++ 2015_03_AudioSigProc_BUFP/week02/gen_sound.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+
 def make_simple_cos():    
     A = .8
     f = 1000
@@ -14,9 +16,22 @@ def make_simple_cos():
     plt.xlabel('time')
     plt.ylabel('amplitude')
     plt.show()
-    
-make_simple_cos()
+    return x
 
+def make_complex_cos():
+    N = 500
+    k = 3
+    n = np.arange(-N/2, N/2)
+    s = np.exp(1j * 2 * np.pi * k *n /N)
+    plt.plot(n, np.real(s))
+    plt.axis([-N/2, N/2-1, -1, 1])
+    plt.show()
+    plt.plot(n, np.imag(s))
+    plt.show()
+    return s
+    
+#make_simple_cos()
+make_complex_cos()
 
 
 
